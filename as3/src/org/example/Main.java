620c7ac396641a70f45acfea6412c4dc2ffda5ea
azamatmerzemkhan <252823@astanait.edu.kz>
2026-01-17 22:32:07 +0500

    ывывыв

--- as3/src/org/example/Main.java
+++ as3/src/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Assigment 3")
-        System.out.println(123456);
+        System.out.println(1234567);
         System.out.println("Hello");
 
     }
